2a27aac3717d3c274479c47df55e749ffe5c8449
thorsten zerha <thorsten.zerha@tastenwerk.com>
2014-06-20 19:59:53 +0200

    api up

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,6 +25,7 @@ module.exports = function(grunt) {
           'build/stylesheets/caminio-auth.min.css': [ 'assets/stylesheets/caminio-auth/authorization.css' ],
           'build/stylesheets/caminio-profiles.min.css': [ 'assets/stylesheets/caminio-profiles/*.css' ],
           'build/stylesheets/caminio-dashboard.min.css': [ 'assets/stylesheets/caminio-dashboard/*.css' ]
+          'build/stylesheets/caminio-api-3rd.min.css': [ 'assets/stylesheets/caminio-api-app/**/*.css' ]
         }
       }
     },
